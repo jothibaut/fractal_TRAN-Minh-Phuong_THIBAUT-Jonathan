f6a51829c5ffe20ffce99a6418344c99f41ab991
Jonathan Thibaut <thibautjonathan@outlook.com>
2018-04-13 16:53:47 +0200

    implémentation fonctions buffer en cours

--- main.c
+++ main.c
@@ -3,10 +3,28 @@
 #include <unistd.h>
 #include "fractal.h"
 
+void buf_init(struct buffer *buf,int n){
+	buf->tab = (sruct fractal *) malloc(n*sizeof(struct fractal));
+	buf->n = n;
+	buf->front = buf->rear = 0;
+	pthread_mutex_init(&(buf->mutex),NULL);
+	sem_init(&(buf->empty),0,n);
+	sem_init(&(buf->full),0,0);
+}
+
+void buf_free(struct buffer *buf){
+	free(buf->tab);
+	free(buf);
+}
+
+void buf_insert(struct buffer *buf,struct fractal *fract){
+}
+
+void buf_remove(struct buffer *buf){
+	
+}
+
 int main()
 {
-    /* TODO */
-    //Coucou
-	printf("%ld \n", sizeof(unsigned int));
     return 0;
 }
